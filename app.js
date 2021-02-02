aa97fc636b81e4fab361d16d7c64cef9d614d826
imrehan <rehanshaikh2156@gmail.com>
2021-02-03 00:22:58 +0530

    controllers and authRoute added

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// database connection
+const dbURI = 'mongodb://localhost:27017/gym-db';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.send('home'));
+app.get('/items', (req, res) => res.send('items'));
+app.use(authRoutes);
