ce71367fd62da5831b2238749e6c7e4a23430e07
imrehan <rehanshaikh2156@gmail.com>
2021-02-06 14:38:54 +0530

    passwords hashed

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator')
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     email: {
         type:String,
@@ -13,6 +14,17 @@ const userSchema = new mongoose.Schema({
         required: [true,'Please enter a password'],
         minlength: [6,'Minimum password length is 6 characters']
     }
+})
+
+// userSchema.post('save', function(doc,next){
+//     console.log('user saved ',doc);
+//     next();
+// })
+
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
 })
 
 const User = mongoose.model('users',userSchema)
