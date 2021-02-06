caf8d6145fea25f82dafd4ca28fbba7f1b1eaac3
imrehan <rehanshaikh2156@gmail.com>
2021-02-06 16:10:11 +0530

    jwt cookies

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // database connection
 const dbURI = 'mongodb://localhost:27017/gym-db';
@@ -18,3 +20,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.send('home'));
 app.get('/items', (req, res) => res.send('items'));
 app.use(authRoutes);
+
+app.get('/set-cookies',(req, res) =>{
+  res.cookie('new-user',false);
+  res.send('you got the cookie')
+})
